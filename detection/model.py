0d5d5e9297639946c5b0586d94c0cbbd432b9b31
Neelapranavi <neelapranavi@gmail.com>
2026-09-09 21:15:02 +0530

    Add binary abnormality detection baseline

---
+++ detection/model.py
@@ -0,0 +1,24 @@
+import torch.nn as nn
+
+
+class SimpleCNN(nn.Module):
+    """Small binary image-classification baseline for research experiments."""
+
+    def __init__(self, in_channels=1, num_classes=2):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels, 32, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(32, 64, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.AdaptiveAvgPool2d((1, 1)),
+        )
+        self.classifier = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        return self.classifier(x.flatten(1))
